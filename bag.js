0875d7ca8f9a9b76e71a16ee69a1c22032090243
IsmailBogac <92868619+IsmailBogac@users.noreply.github.com>
2025-05-11 18:53:47 +0300

    Yeni değişiklikler

--- bag.js
+++ bag.js
@@ -1,18 +1,36 @@
-const bagContainer =document.getElementById("bag-container")
-const storedBag = JSON.parse(localStorage.getItem("bag")) || [];
+const bagContainer =document.getElementById("bag-container");
+var storedBag = JSON.parse(localStorage.getItem("bag")) || [];
+
+if(bagContainer){
+        
+        bagContainer.innerHTML = storedBag.map(
+            (item) =>
+                `    
+            <div class = "product">
+            <img src = "${item.imgUrl}" alt="${item.name}" width = "150px" height="150px"  />
+            <h2 class="name">${item.name}</h2>
+            <p class="price">${item.price}</p>
+            <button class="delete" data-id=${item.id}>Delete</button>
+            </div>
+            
+             ${item.price}
+            ` 
+
+        )
+    }
+
+const deleteBtn = document.querySelectorAll('.delete');
+
+deleteBtn.forEach((btn) => {
+    btn.addEventListener('click',() => {
+        const id = parseInt(btn.dataset.id);
+        const updateBag = storedBag.filter(item => item.id !==id )
+        localStorage.setItem("bag",JSON.stringify(updateBag))
+        location.reload();
+    })
+})
 
 
-const renderCart = (bagList) => {
-    bagContainer.innerHTML = bagList.map(
-        (item) =>
-            `    <div class = "product">
-                <img src = "${item.imgUrl}" alt="${this.name}" width = "150px" height="150px"  />
-                <h2 class="name">${item.name}</h2>
-                <p class="price">${item.price}</p>
-                </div>
-                
-                ` 
-    )
-}
 
-renderCart(storedBag);
+const totalSpan = document.createElement("span");
+
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class products {
-  constructor(name, price, imgUrl) {
+  constructor(id,name, price, imgUrl) {
+    this.id = id;
     this.name = name;
     this.price = price;
     this.imgUrl = imgUrl;
@@ -10,7 +11,7 @@ class products {
             <img src = "${this.imgUrl}" alt="${this.name}" width = "150px" height="150px"  />
             <h2 class="name">${this.name}</h2>
             <p class="price">${this.price}</p>
-            <button  class="addToBag" data-id="${this.name}">Add To Bag</button>
+            <button  class="addToBag" data-id="${this.id}">Add To Bag</button>
             </div>
         `;
   }
@@ -23,55 +24,53 @@ class productList {
   }
   addProduct(product) {
     this.listArr.push(product);
-    localStorage.setItem("list",JSON.stringify(list.listArr))
+    localStorage.setItem("listArr", JSON.stringify(list.listArr));
   }
 }
 
 class bag {
   constructor() {
-      const storedBag= JSON.parse(localStorage.getItem("bag"));
-      this.bagList = storedBag ? storedBag : []
-      this.bagList = [];
-    }
-    
-    addToBag(product) {
-    localStorage.setItem("bag", JSON.stringify(cart.bagList));
+    const storedBag = JSON.parse(localStorage.getItem("bag"));
+    this.bagList = storedBag ? storedBag : [];
+  }
+
+  addToBag(product) {
     this.bagList.push(product);
+    localStorage.setItem("bag", JSON.stringify(this.bagList));
     console.log(product, "sepete eklendi.");
   }
 }
 
-let item1 = new products("Perfume", 100, "item1.png");
-let item2 = new products("jacket", 250, "jacket.jpg");
-let item3 = new products("Blender", 300, "blender.jpg");
-let item4 = new products("Book", 150, "book.png");
-
+let item1 = new products(1,"Perfume", 100, "item1.png");
+let item2 = new products(2,"jacket", 250, "jacket.jpg");
+let item3 = new products(3,"Blender", 300, "blender.jpg");
+let item4 = new products(4,"Book", 150, "book.png");
 
 const list = new productList();
 const cart = new bag();
 const productsContainer = document.getElementById("products-container");
-localStorage.setItem("listArr", JSON.stringify(list.listArr));
+
 
 list.addProduct(item1);
 list.addProduct(item2);
 list.addProduct(item3);
 list.addProduct(item4);
 
+localStorage.setItem("listArr", JSON.stringify(list.listArr));
+
 list.listArr.map((item) => {
   productsContainer.innerHTML += item.renderAll();
 });
 
 const bagButton = document.querySelectorAll(".addToBag");
-const bagContainer = document.getElementById("bag-container");
 
 bagButton.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    const name = e.target.dataset.id;
+    const id =parseInt(e.target.dataset.id);
 
     const selectedProduct = list.listArr.find((item) => {
-      return item.name == name;
+      return item.id == id;
     });
     cart.addToBag(selectedProduct);
-    // bagContainer.innerHTML = cart.renderAll();
   });
 });
